ee9e43e43b497627fed57b915479b04e85caaf9f
BSathwi <bojjasathwik1234@gmail.com>
2025-05-18 15:33:40 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const movieRoutes = require('./routes/movies');
+const movieRoutes = require('../routes/movies');
 
 const app = express();
 
